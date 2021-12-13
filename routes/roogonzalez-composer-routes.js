4f698d27751b7a8911d1e8524c112add97b94090
goostavo25 <groogonzalez@my365.bellevue.edu>
2021-12-12 22:57:27 -0500

    Added Week 8 Assignment

--- routes/roogonzalez-composer-routes.js
+++ routes/roogonzalez-composer-routes.js
@@ -122,4 +122,98 @@ router.get("/composers/:id", (req, res) => {
   }
 });
 
+/**
+ * @openapi
+ * /api/composers/{id}:
+ *   put:
+ *     summary: Modify the composer object
+ *     description: Modify the composer object
+ *     tags: [composer]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description:
+ *           the composer id requested by the user
+ *         required: true
+ *     requestBody:
+ *       description:
+ *         Composer's Information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: "object"
+ *             properties:
+ *               firstName:
+ *                 type: "string"
+ *               lastName:
+ *                 type: "string"
+ *     responses:
+ *       200:
+ *         description: array of composer documents
+ *       401:
+ *         description: Invalid composerID
+ *       500:
+ *         description: Server Exception
+ *       501:
+ *         description: MongoDB Exception
+ */
+
+router.put("/composers/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    Composer.findOne({ _id: id }, function (error, composer) {
+      if (error) res.status(501).send("MongoDB exception");
+      if (composer) {
+        composer.set({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+        });
+        composer.save((error, composer) => {
+          if (error) res.status(501).send("MongoDB exception");
+          res.send(composer);
+        });
+      }
+      if (!composer) {
+        res.status(401).send("Invalid composerID");
+      }
+    });
+  } catch (error) {
+    res.status(500).send("server exception");
+  }
+});
+
+/**
+ * @openapi
+ * /api/composers/{id}:
+ *   delete:
+ *     summary: Delete the composer object
+ *     description: Delete the composer object
+ *     tags: [composer]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description:
+ *           the composer id requested by the user
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: array of composer documents
+ *       500:
+ *         description: Server Exception
+ *       501:
+ *         description: MongoDB Exception
+ */
+
+router.delete("/composers/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    Composer.findByIdAndDelete({ _id: id }, function (error, composer) {
+      if (error) res.status(501).send("MongoDB exception");
+      res.send(composer);
+    });
+  } catch (error) {
+    res.status(500).send("server exception");
+  }
+});
+
 module.exports = router;
